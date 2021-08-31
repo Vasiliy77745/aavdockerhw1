1529dcd4fd75bdc59903ae1e62ddc374de9afa55
Alex V <alekceyanatolievich@gmail.com>
2021-08-31 12:24:08 +0300

    change code docker-compose.yaml and add pipeline.sh

---
+++ pipeline.sh
@@ -0,0 +1,16 @@
+!#usr/bin/bash
+
+mkdir ~/dockertest1 && cd ~/dockertest1
+
+git init .
+
+git pull git@github.com:Vasiliy77745/aavdockerhw1.git
+
+export TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+
+docker build -t alexv77745/hw2:$TAG .
+
+docker-compose up -d
+
+docker push alexv77745/hw2:$TAG
+
